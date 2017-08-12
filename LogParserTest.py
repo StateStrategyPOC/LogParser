90d975ece014f9602f222f22101e2e6c51e6fc03
giorgiopea <annatar93@gmail.com>
2017-08-12 17:59:21 +0200

    fixed for better testing integration

--- LogParserTest.py
+++ LogParserTest.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 from LogParser import LogParser
-import unittest
+from unittest import TestCase
 
 
-class TestLogParser(unittest.TestCase):
+class TestLogParser(TestCase):
     def test_parse(self):
         LogParser("./test_log.txt", ".", "@ERROR_GROUP")
-        with open("./test_log.text_err_timestamps.txt") as f:
+        with open("./test_log.txt_err_timestamps.txt") as f:
             lines = f.readlines()
             self.assertEquals(len(lines), 1)
             self.assertEquals(lines[0], "1502443757149")
 
 
-if __name__ == "__main__":
-    unittest.main()
